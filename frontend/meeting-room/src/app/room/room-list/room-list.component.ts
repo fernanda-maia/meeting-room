858f1fe92f3d8b0bf008ccdc8fa355a68a71add8
Fernanda Ribeiro <fer.maiafr@gmail.com>
2021-08-11 19:42:50 -0300

    List Room Template

--- frontend/meeting-room/src/app/room/room-list/room-list.component.ts
+++ frontend/meeting-room/src/app/room/room-list/room-list.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+
+
+const actions = ["delete", "view"];
+const ROOM_DATA = [
+  {name: "Sala 01", date: "10/10/2021", start: "10:00:00", end: "12:00:00", actions},
+  {name: "Sala 02", date: "10/09/2021", start: "12:00:00", end: "19:00:00", actions},
+  {name: "Sala 02", date: "10/09/2021", start: "12:00:00", end: "19:00:00", actions},
+  {name: "Sala 02", date: "10/09/2021", start: "12:00:00", end: "19:00:00", actions}
+]
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
   styleUrls: ['./room-list.component.css']
 })
-export class RoomListComponent implements OnInit {
+export class RoomListComponent implements AfterViewInit, OnInit {
+  columns: string[] = [
+    "name",
+    "date",
+    "start",
+    "end",
+    "actions"
+  ];
 
-  constructor() { }
+  source = new MatTableDataSource(ROOM_DATA);
+
+  @ViewChild(MatSort)
+  sort: MatSort;
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
+    
   }
+
+  ngAfterViewInit(): void {
+    this.source.sort = this.sort;
+  }
+
 
 }
--- frontend/meeting-room/src/app/views/not-found-screen/not-found-screen.component.ts
+++ frontend/meeting-room/src/app/views/not-found-screen/not-found-screen.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-not-found-screen',
